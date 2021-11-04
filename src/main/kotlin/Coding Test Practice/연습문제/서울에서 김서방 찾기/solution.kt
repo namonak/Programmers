6ff4bb4cfaf9f7bbf91f4ea1649c1aba29b69258
namonak <namonak@gmail.com>
2021-11-04 17:37:33 +0900

    서울에서 김서방 찾기 : https://programmers.co.kr/learn/courses/30/lessons/12919
    Help

---
+++ src/main/kotlin/Coding Test Practice/연습문제/서울에서 김서방 찾기/solution.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(seoul: Array<String>): String {
+        var answer = ""
+        for (i in seoul.indices) {
+            if (seoul[i] == "Kim") answer = "김서방은 ${i}에 있다"
+        }
+        return answer
+    }
+}
