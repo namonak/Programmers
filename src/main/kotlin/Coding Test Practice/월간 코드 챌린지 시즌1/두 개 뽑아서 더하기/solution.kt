127e993155ff8591e541b4f8f461e5b614f42f4d
namonak <namonak@gmail.com>
2022-02-08 23:58:19 +0900

    두 개 뽑아서 더하기 : https://programmers.co.kr/learn/courses/30/lessons/68644

---
+++ src/main/kotlin/Coding Test Practice/월간 코드 챌린지 시즌1/두 개 뽑아서 더하기/solution.kt
@@ -0,0 +1,19 @@
+package `Coding Test Practice`.`월간 코드 챌린지 시즌1`.`두 개 뽑아서 더하기`
+
+class Solution {
+    fun solution(numbers: IntArray): IntArray {
+        var answer = mutableListOf<Int>()
+
+        for (i in numbers.indices) {
+            for (j in i + 1 until numbers.size) {
+                if (!answer.contains(numbers[i] + numbers[j])) {
+                    answer.add(numbers[i] + numbers[j])
+                }
+            }
+        }
+
+        answer.sort()
+
+        return answer.toIntArray()
+    }
+}
